b17d13a6c4742631c93b2173e0f5237e78bb3017
ShahRustam <shahrustam98@gmail.com>
2020-03-04 17:40:04 +0200

    Init package. // Resolve #1 (#5)
    * Init python package. Resolve #1.
    
    * remove unused requires from setup.py
    
    * Fixes
    
    * minor fixes

---
+++ pioinstaller/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+VERSION = (0, 0, 0)
+__version__ = ".".join([str(s) for s in VERSION])
+
+__title__ = "platformio-installer"
+__description__ = "An installer for PlatformIO Core"
+
+__url__ = "https://platformio.org"
+
+__author__ = "PlatformIO"
+__email__ = "contact@platformio.org"
+
+__license__ = "Apache Software License"
+__copyright__ = "Copyright 2014-present PlatformIO"
