6224fbb001d6269db677fe30a9d5cfd0ecd9b496
Shahrustam <shahrustam98@gmail.com>
2020-04-06 12:56:31 +0300

    fix namings.

--- pioinstaller/core.py
+++ pioinstaller/core.py
@@ -130,6 +130,7 @@ def check(dev=False, auto_upgrade=False, version_requirements=None):
     python_exe = os.path.join(
         penv.get_penv_bin_dir(), "python.exe" if util.IS_WINDOWS else "python"
     )
+    result = {}
 
     if not os.path.isfile(platformio_exe):
         raise exception.InvalidPlatformIOCore(
@@ -143,16 +144,16 @@ def check(dev=False, auto_upgrade=False, version_requirements=None):
         )
 
     try:
-        pio_state = fetch_python_state(python_exe)
+        result.update(fetch_python_state(python_exe))
     except subprocess.CalledProcessError as e:
         error = e.output.decode()
         raise exception.InvalidPlatformIOCore(
             "Could not import PlatformIO module. Error: %s" % error
         )
 
-    pio_version = convert_version(pio_state.get("core_version"))
-    dev = dev or bool(pio_version.prerelease if pio_version else False)
-    pio_state.update(
+    piocore_version = convert_version(result.get("core_version"))
+    dev = dev or bool(piocore_version.prerelease if piocore_version else False)
+    result.update(
         {
             "core_dir": get_core_dir(),
             "cache_dir": get_cache_dir(),
@@ -168,10 +169,10 @@ def check(dev=False, auto_upgrade=False, version_requirements=None):
 
     if version_requirements:
         try:
-            if pio_version in semantic_version.Spec(version_requirements):
+            if piocore_version in semantic_version.Spec(version_requirements):
                 raise exception.InvalidPlatformIOCore(
                     "PlatformIO Core version %s does not match version requirements %s."
-                    % (str(pio_version), version_requirements)
+                    % (str(piocore_version), version_requirements)
                 )
         except ValueError:
             click.secho(
@@ -197,7 +198,7 @@ def check(dev=False, auto_upgrade=False, version_requirements=None):
         )
 
     if not auto_upgrade:
-        return pio_state
+        return result
 
     time_now = int(round(time.time()))
 
@@ -207,22 +208,22 @@ def check(dev=False, auto_upgrade=False, version_requirements=None):
         last_piocore_version_check
         and (time_now - int(last_piocore_version_check)) < UPDATE_INTERVAL
     ):
-        return pio_state
+        return result
 
     with open(os.path.join(penv.get_penv_dir(), "state.json"), "w") as fp:
         penv_state["last_piocore_version_check"] = time_now
         json.dump(penv_state, fp)
 
     if not last_piocore_version_check:
-        return pio_state
+        return result
 
     upgrade_core(platformio_exe, dev)
 
     try:
-        pio_state.update(fetch_python_state(python_exe))
+        result.update(fetch_python_state(python_exe))
     except:  # pylint:disable=bare-except
         raise exception.InvalidPlatformIOCore("Could not import PlatformIO module")
-    return pio_state
+    return result
 
 
 def fetch_python_state(python_exe):
